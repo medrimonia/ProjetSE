173c9224f38f3b0409703daf656c0e6afcdcbe8a
medrimonia <medrimonia@gmail.com>
2013-11-27 15:21:50 +0100

    First rework on the protocol

---
+++ include/commands.h
@@ -0,0 +1,19 @@
+#ifndef COMMANDS_H
+#define COMMANDS_H
+
+typedef int8_t command_no
+
+#define CMD_GET_CAPS       0
+#define CMD_RESET          1
+#define CMD_PING           2
+#define CMD_READ           3
+#define CMD_WRITE          4
+#define CMD_GET_TYPE       5
+#define CMD_SET_TYPE       6
+#define CMD_GET_FAILSAFE   7
+#define CMD_SET_FAILSAFE   8
+// This command number is reserved for the specific purpose of spontaneous
+// messages from the device in a future version
+#define CMD_RESERVED     15
+
+#endif//COMMANDS_H
---
+++ include/types.h
@@ -0,0 +1,13 @@
+#ifndef TYPES_H
+#define TYPES_H
+
+//TYPE sur 3 bits
+#define PIN_TYPE_ANALOG8       0
+#define PIN_TYPE_ANALOG16      1
+#define PIN_TYPE_DIGITAL       2
+#define PIN_TYPE_PWM8          3
+#define PIN_TYPE_PWM16         4
+#define PIN_TYPE_USE_STATE     7
+#define PIN_TYPE_DISABLED      7
+
+#endif//TYPES_H
