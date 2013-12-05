9b3cda8e4ea74e57710a867020016daf25857eab
medrimonia <medrimonia@gmail.com>
2013-12-05 21:57:56 +0100

    analogic write pwm8 and pwm16 tested

--- src/test_driver.c
+++ src/test_driver.c
@@ -31,8 +31,14 @@ int main(void){
   m[8] = MASK_PIN_ON;
   get_type_mask(c, &m, NULL);
   free(m);
-  printf("Digital write of 'on' on pin 1 : ");
-  digital_write(c, 1 , true);
+  printf("Digital write of  'on' on pin 1 : ");
+  digital_write (c, 1, true);
+  printf("Analog  write of '510' on pin 6 : ");
+  analogic_write(c, 6,  510);
+  printf("PWM8    write of  '24' on pin 3 : ");
+  pwm8_write    (c, 3,   24);
+  printf("PWM16   write of '515' on pin 9 : ");
+  pwm16_write   (c, 9,  515);
   destroy_connection(c);
   exit(EXIT_SUCCESS);
 }
