7d41b0c8f7dc5bf00781438fc3c49ef87857324e
medrimonia <medrimonia@gmail.com>
2013-12-14 22:53:56 +0100

    Switching test_communication to assertions and fixing a problem when fd wasn't open

--- driver/tests/test_communication.c
+++ driver/tests/test_communication.c
@@ -1,22 +1,34 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+#include <assert.h>
+
 #include "bit_utils.h"
 #include "connection.h"
 #include "driver.h"
+
+#define NB_PINS 16
 
 struct connection * c = NULL;
 
 void check_connection_caps()
 {
+#ifdef DEBUG
   printf( "Read number of pins     : %d\n", c->caps.nb_pins );
   printf( "Read type mask          : " );
   display_packet_hexa( c->caps.pins_mask_type, c->caps.nb_pins );
   printf( "\n" );
+#endif
+  assert( c->caps.nb_pins == NB_PINS );
+  unsigned int i;
+  for ( i = 0; i < NB_PINS; i++ ) {
+    assert( c->caps.pins_mask_type[i] == 31 );//TODO hard-coded value
+  }
 }
 
 int main(void){
   c = connection_open( DEV2DRIV_FILENAME, DRIV2DEV_FILENAME );
+  if ( c == NULL ) return EXIT_FAILURE;
   printf( "Connection initialized, sending getCaps :\n" );
   get_caps( c );
   // Tests
